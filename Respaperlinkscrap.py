e95f6c44cc29de0155d55a29ea15c90c52aa5550
Subhrajit Guchait <subhrajitguchait20@gmail.com>
2022-07-23 23:05:30 +0530

    build: v.0.5.0

--- Respaperlinkscrap.py
+++ Respaperlinkscrap.py
@@ -12,6 +12,9 @@ from time import sleep
 from random import randrange
 import pandas as pd
 
+def to_raw(string):
+    return fr"{string}"
+
 # REVIEW: Build a Web Scraper to fetch Research Paper links and short summary on the papers and their 
 # REVIEW: authors that matches the topics given in the command line input.
 # REVIEW: from the Science Direct website using python and selenium
@@ -49,43 +52,63 @@ except:
 # Wait for the page to load
 sleep(randrange(10,15))# seconds
 
+bttn_Xpath = "/html/body/div[1]/div/div/div/div/div/div/section/div/div[2]/div[2]/div[1]/div[2]/div[3]/div[1]/ol/li[3]/a"
+driver.find_element(By.XPATH, value=bttn_Xpath).click()
+
+# Wait for the page to load
+sleep(randrange(25,30))# seconds
+
 ## Intitialize empty lists to store data
 articles_type = []
 titles = []
-# authors = []
+authors = []
 journals = []
 # journal_infos = []
-# urls = []
+urls = []
 # Research paper digital object identifier[doi] link
 doi_links = []
+
 # Collect all the info in articles:
-article_containers = driver.find_elements(By.ID, 'srp-results-list')
+article_containers = driver.find_elements(By.CSS_SELECTOR, '.ResultItem col-xs-24 push-m'.replace(' ', '.'))
 
 for article in article_containers:
     try:
         article_type = article.find_element(By.CSS_SELECTOR, '.article-type u-clr-grey8'.replace(' ', '.')).text
         title = article.find_element(By.CSS_SELECTOR, '.anchor result-list-title-link u-font-serif text-s anchor-default'.replace(' ', '.')).text
         journal = article.find_element(By.CSS_SELECTOR, '.anchor subtype-srctitle-link anchor-default anchor-has-inherit-color'.replace(' ', '.')).text
-        # journal_info = article.find_element().text
-        # author = article.find_element().text
-        # url = article.find_element().text
-        doi_link = article.get_attribute('data-doi')
+        # FIXME: Get the Journal info
+        # journal_info = article.find_elements(By)
+        # journo = []
+        # for j in journal_info:
+        #     journo.append(j)
+        # journ_inf = ", ".join(journo)
+        # FIXME: Get the Author List displayed on the website 
+        # Working on the authors list.
+        article_auth = []
+        author = article.find_elements(By.TAG_NAME, 'li')
+        for auth in author:
+            if auth != 'Download PDF': # Removing some unnecessary stuffs by Hardcoding 
+                article_auth.append(auth.text)
+        art_auth = ", ".join(article_auth)
+
+        url = to_raw(article.find_element(By.CSS_SELECTOR, '.anchor result-list-title-link u-font-serif text-s anchor-default'.replace(' ', '.')).get_attribute('href'))
+        # FIXME: doi_link is not getting scraped** Need a FIX
+        doi_link = article.find_element(By.TAG_NAME, 'a').get_attribute('data-doi')
         articles_type.append(article_type)
         titles.append(title)
         journals.append(journal)
         # journal_infos.append(journal_info)
-        # urls.append(url)
+        authors.append(art_auth)
+        urls.append(url)
         doi_links.append(doi_link)
     except Exception as err:
         print(f'Error while scraping data: {err}')
         continue
 
+    article_container_df = pd.DataFrame({'article_type': articles_type, 'title': titles, 'journal': journals, 'authors':authors, 'url': urls, 'doi-link': doi_links})
+# article_container_df = pd.DataFrame({'article_type': articles_type, 'title': titles, 'journal': journals, 'doi-link': doi_links})
 
-article_container_df = pd.DataFrame({'article_type': articles_type, 'title': titles, 'journal': journals, 'doi-link': doi_links})
-
-# article_container_df = pd.DataFrame({'article_type': articles_type, 'title': titles, 'journal': journals, 
-# 'journal_info': journal_infos, 'url': urls, 'doi-link': doi_links})
-
+# OPTIMIZE: Task:-1 Optimize and clean the entire code using classes and functions.
 # TODO: Add paginations for extracting articles from other pages as well.
 
 # TODO: Exporting the results to a .json file.
