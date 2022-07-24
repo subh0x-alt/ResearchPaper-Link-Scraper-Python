8c8031e08977cedb3f30c8f0ae124f3f519fab5d
Subhrajit Guchait <subhrajitguchait20@gmail.com>
2022-07-24 10:14:31 +0530

    build: v.1.0.0

--- Respaperlinkscrap.py
+++ Respaperlinkscrap.py
@@ -52,20 +52,21 @@ except:
     print('Text Search Button not found!')
 
 # Wait for the page to load
-sleep(randrange(5,8))# seconds
+sleep(randrange(1,3))# seconds
 
 # bttn_Xpath = "/html/body/div[1]/div/div/div/div/div/div/section/div/div[2]/div[2]/div[1]/div[2]/div[3]/div[1]/ol/li[3]/a"
 bttn_Xpath = "//a[@data-aa-name='srp-100-results-per-page']"
 driver.find_element(By.XPATH, value=bttn_Xpath).click()
 
 # Wait for the page to load
-sleep(randrange(5,10))# seconds
+sleep(randrange(3,6))# seconds
 
 ## Intitialize empty lists to store data
 articles_type = []
 titles = []
 authors = []
 journals = []
+dates = []
 journal_infos = []
 urls = []
 # Research paper digital object identifier[doi] link
@@ -79,6 +80,7 @@ for article in article_containers:
         article_type = article.find_element(By.CSS_SELECTOR, '.article-type u-clr-grey8'.replace(' ', '.')).text
         title = article.find_element(By.CSS_SELECTOR, '.anchor result-list-title-link u-font-serif text-s anchor-default'.replace(' ', '.')).text
         journal = article.find_element(By.CSS_SELECTOR, '.anchor subtype-srctitle-link anchor-default anchor-has-inherit-color'.replace(' ', '.')).text
+        date = article.find_element(By.XPATH, "//span[@class='srctitle-date-fields']/span[2]").text
         # FIXME: Get the Journal info
         article.find_element(By.XPATH, "//button [@data-aa-button='srp-show-hidden-volume-issue-info']").click()
         journal_info = article.find_elements(By.XPATH, "//span[@class='hidden-fields']")
@@ -99,6 +101,7 @@ for article in article_containers:
         articles_type.append(article_type)
         titles.append(title)
         journals.append(journal)
+        dates.append(date)
         journal_infos.append(journo)
         authors.append(art_auth)
         urls.append(url)
@@ -107,7 +110,7 @@ for article in article_containers:
         print(f'Error while scraping data: {err}')
         continue
 
-    article_container_df = pd.DataFrame({'article-type': articles_type, 'title': titles, 'journal': journals, 'journal-info': journal_infos, 'authors':authors, 'url': urls, 'doi-link': doi_links})
+    article_container_df = pd.DataFrame({'article-type': articles_type, 'title': titles, 'journal': journals, 'published-on':dates, 'journal-info': journal_infos, 'authors':authors, 'url': urls, 'doi-link': doi_links})
 # article_container_df = pd.DataFrame({'article_type': articles_type, 'title': titles, 'journal': journals, 'doi-link': doi_links})
 
 # OPTIMIZE: Task:-1 Optimize and clean the entire code using classes and functions.
